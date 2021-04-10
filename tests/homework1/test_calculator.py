51b11555947f06ff449d2da1f4309311b8b1b6d6
Andrey Borisevich <tinker000@yandex.ru>
2021-04-10 15:00:05 +0300

    Reformat the test

--- tests/homework1/test_calculator.py
+++ tests/homework1/test_calculator.py
@@ -2,14 +2,19 @@
 """Test homework1/calculator/calc
 """
 
+
 from homework1.calc import check_power_of_2
 
 
 def test_positive_case():
     """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
+    assert check_power_of_2(65536), "positive case fail"
 
 
 def test_negative_case():
     """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)
+    assert not check_power_of_2(12), "negative case fail"
+
+
+test_positive_case()
+test_negative_case()
