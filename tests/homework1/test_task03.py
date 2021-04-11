7e0386aa525c6ac3b729a8eba2d1d2422f681e12
Andrey Borisevich <tinker000@yandex.ru>
2021-04-11 17:56:45 +0300

    Task03 completed

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -5,21 +5,16 @@
 
 from homework1.task03 import find_maximum_and_minimum
 
-
-PATH = 'test_task_3_cases/'
+PATH = "tests/homework1/test_task_3_cases/"
 
 
 def test_2_num_cases():
     """Testing if file has 2 integers, positive cases"""
-    assert find_maximum_and_minimum(PATH +'case1.txt') == (1, 1),\
-        'case1 fail'
-    assert find_maximum_and_minimum(PATH + 'case2.txt') == (-100, 100),\
-        'case2 fail'
+    assert find_maximum_and_minimum(PATH + "case1.txt") == (1, 1), "case1 fail"
+    assert find_maximum_and_minimum(PATH + "case2.txt") == (-100, 100), "case2 fail"
 
 
 def test_many_num_cases():
     """Testing if file has more then 2 integers, positive cases"""
-    assert find_maximum_and_minimum(PATH + 'case3.txt') == (0, 0), \
-        'case3 fail'
-    assert find_maximum_and_minimum(PATH + 'case4.txt') == (-10, 10), \
-        'case4 fail'
+    assert find_maximum_and_minimum(PATH + "case3.txt") == (0, 0), "case3 fail"
+    assert find_maximum_and_minimum(PATH + "case4.txt") == (-10, 10), "case4 fail"
