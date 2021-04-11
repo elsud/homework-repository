bb08ea8d5291ec2db23ec3b5bfaa0df023e381b5
Andrey Borisevich <tinker000@yandex.ru>
2021-04-11 20:05:40 +0300

    Task04 completed

---
+++ homework1/task04.py
@@ -0,0 +1,30 @@
+"""
+Classic task, a kind of walnut for you
+
+Given four lists A, B, C, D of integer values,
+    compute how many tuples (i, j, k, l) there are such that
+    A[i] + B[j] + C[k] + D[l] is zero.
+
+We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
+"""
+from typing import List
+
+
+def check_sum_of_four(
+    list_a: List[int], list_b: List[int], list_c: List[int], list_d: List[int]
+) -> int:
+    """This function count number of tuples (i, j, k, l) there are such that
+    a[i] + b[j] + c[k] + d[l] equal zero"""
+    count_tuples = 0
+    sums = {}
+    for a_value in list_a:
+        for b_value in list_b:
+            if a_value + b_value not in sums:
+                sums[a_value + b_value] = 1
+            else:
+                sums[a_value + b_value] += 1
+    for c_value in list_c:
+        for d_value in list_d:
+            if -(c_value + d_value) in sums:
+                count_tuples += sums[-(c_value + d_value)]
+    return count_tuples
---
+++ tests/homework1/test_task04.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Test homework1/task04.py
+"""
+
+from homework1.task04 import check_sum_of_four
+from random import randint
+from time import time
+
+
+def test_correctness_with_no_values():
+    """Test with length of list equal to 0"""
+    assert check_sum_of_four([], [], [], []) == 0
+
+
+def test_correctness_with_1_value():
+    """Test with length of list equal to 1"""
+    assert check_sum_of_four([1], [2], [3], [4]) == 0
+    assert check_sum_of_four([-1], [-20], [1], [20]) == 1
+
+
+def test_correctness_with_many_values():
+    """Test with length of list more than 1"""
+    assert check_sum_of_four([-1, 1], [-2, 2], [2, -2], [1, -1]) == 4
+    assert check_sum_of_four([0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]) == 81
+
+
+def test_time_to_solve():
+    """Test that the time to solve the problem is not very long"""
+    a = [randint(-5 * 10 ** 9, 5 * 10 ** 9) for _ in range(200)]
+    b = [randint(-5 * 10 ** 9, 5 * 10 ** 9) for _ in range(200)]
+    c = [randint(-5 * 10 ** 9, 5 * 10 ** 9) for _ in range(200)]
+    d = [randint(-5 * 10 ** 9, 5 * 10 ** 9) for _ in range(200)]
+    start_time = time()
+    check_sum_of_four(a, b, c, d)
+    time_to_solve_with_1000_members = (time() - start_time) * 5 ** 4
+    assert time_to_solve_with_1000_members < 1800
