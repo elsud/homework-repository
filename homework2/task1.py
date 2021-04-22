616cd89b5c01702b8b9cd7c23f569df000dca43f
elsud <dymskayaalena@gmail.com>
2021-04-22 16:16:30 +0300

    Make task1 less repeated.

--- homework2/task1.py
+++ homework2/task1.py
@@ -9,27 +9,20 @@ Given a file containing text. Complete using only default collections:
 from typing import List
 
 
-def reading_text_in_lines(file_path: str) -> List[str]:
+def reading_text(file_path: str) -> List[str]:
     """Reading text from file in lines."""
     with open(file_path) as data:
-        text = data.readlines()
-    return text
-
-
-def reading_text(file_path: str) -> str:
-    """reading text from file."""
-    with open(file_path) as data:
-        text = data.read()
+        text = (line.encode().decode("unicode_escape") for line in data.readlines())
     return text
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
     """Search ten words with longest set of unique chars."""
-    text = reading_text_in_lines(file_path)
+    text = reading_text(file_path)
     punctuation = (".", ":", ",", "?", "!")
     longest_words = []
     for line in text:
-        line = line.strip().encode().decode("unicode_escape").split()
+        line = line.strip().split()
         for word in line:
             while word.endswith(punctuation):
                 word = word[:-1]
@@ -53,9 +46,8 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
 def get_rarest_char(file_path: str) -> str:
     """Counting frequency of every char in text and search the rarest one."""
     frequency = {}
-    text = reading_text_in_lines(file_path)
+    text = reading_text(file_path)
     for line in text:
-        line = line.encode().decode("unicode_escape")
         for char in line:
             if char in frequency:
                 frequency[char] += 1
@@ -73,16 +65,18 @@ def count_punctuation_chars(file_path: str) -> int:
     """Counting all punctuations chars in text."""
     punctuations = [",", ".", ":", ";", "?", "!", "(", ")", "—"]
     acc = 0
-    text = reading_text(file_path).encode().decode("unicode_escape")
-    for char in text:
-        if char in punctuations:
-            acc += 1
+    text = reading_text(file_path)
+    for line in text:
+        for char in line:
+            if char in punctuations:
+                acc += 1
     return acc
 
 
 def count_non_ascii_chars(file_path: str) -> int:
     """Counting all non ascii chars in text."""
-    text = reading_text(file_path)
+    with open(file_path) as data:
+        text = data.read()
 
     return text.count("\\")
 
@@ -90,9 +84,8 @@ def count_non_ascii_chars(file_path: str) -> int:
 def get_most_common_non_ascii_char(file_path: str) -> str:
     """Counting frequenty of every non ascii char and search most common."""
     frequency = {}
-    text = reading_text_in_lines(file_path)
+    text = reading_text(file_path)
     for line in text:
-        line = line.strip().encode().decode("unicode_escape")
         for char in line:
             if not char.isascii():
                 if char in frequency:
