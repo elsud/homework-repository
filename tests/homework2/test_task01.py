3870f30b7e476f9f7390bb5fc07a36b8df73f3f0
Sophiabattler <79052071542@yandex.ru>
2021-05-03 01:26:41 +0300

    changes in test_task01.py

--- tests/homework2/test_task01.py
+++ tests/homework2/test_task01.py
@@ -1,11 +1,7 @@
 """Test for task01 - Text work"""
-from homework2.task01 import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+from homework2.task01 import (count_non_ascii_chars, count_punctuation_chars,
+                              get_longest_diverse_words,
+                              get_most_common_non_ascii_char, get_rarest_char)
 
 
 def test_most_unique_longest_words():
